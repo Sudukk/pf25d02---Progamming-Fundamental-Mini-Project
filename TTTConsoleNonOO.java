975bc5a48b973b1d7a6f2c7ae378236a83699962
sereinne <akbarpersonalmode@gmail.com>
2025-06-03 12:18:21 +0700

    keren!

--- TTTConsoleNonOO.java
+++ TTTConsoleNonOO.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
+
 /**
  * Tic-Tac-Toe: Two-player, console-based, non-graphics, non-OO version.
  * All variables/methods are declared as static (i.e., class)
  *  in this non-OO version.
  */
 public class TTTConsoleNonOO {
+
     // Define named constants for:
     //  1. Player: using CROSS and NOUGHT
     //  2. Cell contents: using CROSS, NOUGHT and NO_SEED
-    public static final int CROSS   = 0;
-    public static final int NOUGHT  = 1;
+    public static final int CROSS = 0;
+    public static final int NOUGHT = 1;
     public static final int NO_SEED = 2;
 
     // The game board
-    public static final int ROWS = 3, COLS = 3;  // number of rows/columns
+    public static final int ROWS = 3, COLS = 3; // number of rows/columns
     public static int[][] board = new int[ROWS][COLS]; // EMPTY, CROSS, NOUGHT
 
     // The current player
-    public static int currentPlayer;  // CROSS, NOUGHT
+    public static int currentPlayer; // CROSS, NOUGHT
 
     // Define named constants to represent the various states of the game
-    public static final int PLAYING    = 0;
-    public static final int DRAW       = 1;
-    public static final int CROSS_WON  = 2;
+    public static final int PLAYING = 0;
+    public static final int DRAW = 1;
+    public static final int CROSS_WON = 2;
     public static final int NOUGHT_WON = 3;
     // The current state of the game
     public static int currentState;
@@ -52,41 +54,68 @@ public class TTTConsoleNonOO {
             // Switch currentPlayer
             currentPlayer = (currentPlayer == CROSS) ? NOUGHT : CROSS;
         } while (currentState == PLAYING); // repeat if not game over
+        boolean invalid = true;
+        do {
+            System.out.println("Play Again (y/n)?");
+            char ans = in.next().charAt(0);
+            if (ans == 'y' || ans == 'Y') {
+                invalid = false;
+            } else if (ans == 'n' || ans == 'N') {
+                System.out.println("Bye!");
+                System.exit(0);
+            } else {
+                System.out.println("invalid character!");
+            }
+        } while (invalid);
     }
 
     /** Initialize the board[][], currentState and currentPlayer for a new game*/
     public static void initGame() {
         for (int row = 0; row < ROWS; ++row) {
             for (int col = 0; col < COLS; ++col) {
-                board[row][col] = NO_SEED;  // all cells empty
+                board[row][col] = NO_SEED; // all cells empty
             }
         }
-        currentPlayer = CROSS;   // cross plays first
-        currentState  = PLAYING; // ready to play
+        currentPlayer = CROSS; // cross plays first
+        currentState = PLAYING; // ready to play
     }
 
     /** The currentPlayer makes one move (one step).
      Update board[selectedRow][selectedCol] and currentState. */
     public static void stepGame() {
-        boolean validInput = false;  // for input validation
+        boolean validInput = false; // for input validation
         do {
             if (currentPlayer == CROSS) {
-                System.out.print("Player 'X', enter your move (row[1-3] column[1-3]): ");
+                System.out.print(
+                    "Player 'X', enter your move (row[1-3] column[1-3]): "
+                );
             } else {
-                System.out.print("Player 'O', enter your move (row[1-3] column[1-3]): ");
+                System.out.print(
+                    "Player 'O', enter your move (row[1-3] column[1-3]): "
+                );
             }
-            int row = in.nextInt() - 1;  // array index starts at 0 instead of 1
+            int row = in.nextInt() - 1; // array index starts at 0 instead of 1
             int col = in.nextInt() - 1;
-            if (row >= 0 && row < ROWS && col >= 0 && col < COLS
-                    && board[row][col] == NO_SEED) {
+            if (
+                row >= 0 &&
+                row < ROWS &&
+                col >= 0 &&
+                col < COLS &&
+                board[row][col] == NO_SEED
+            ) {
                 // Update board[][] and return the new game state after the move
                 currentState = stepGameUpdate(currentPlayer, row, col);
-                validInput = true;  // input okay, exit loop
+                validInput = true; // input okay, exit loop
             } else {
-                System.out.println("This move at (" + (row + 1) + "," + (col + 1)
-                        + ") is not valid. Try again...");
+                System.out.println(
+                    "This move at (" +
+                    (row + 1) +
+                    "," +
+                    (col + 1) +
+                    ") is not valid. Try again..."
+                );
             }
-        } while (!validInput);  // repeat if input is invalid
+        } while (!validInput); // repeat if input is invalid
     }
 
     /**
@@ -96,25 +125,31 @@ public class TTTConsoleNonOO {
      * new game state (PLAYING, DRAW, CROSS_WON, NOUGHT_WON).
      * @return new game state
      */
-    public static int stepGameUpdate(int player, int selectedRow, int selectedCol) {
+    public static int stepGameUpdate(
+        int player,
+        int selectedRow,
+        int selectedCol
+    ) {
         // Update game board
         board[selectedRow][selectedCol] = player;
 
         // Compute and return the new game state
-        if (board[selectedRow][0] == player       // 3-in-the-row
-                && board[selectedRow][1] == player
-                && board[selectedRow][2] == player
-                || board[0][selectedCol] == player // 3-in-the-column
-                && board[1][selectedCol] == player
-                && board[2][selectedCol] == player
-                || selectedRow == selectedCol      // 3-in-the-diagonal
-                && board[0][0] == player
-                && board[1][1] == player
-                && board[2][2] == player
-                || selectedRow + selectedCol == 2  // 3-in-the-opposite-diagonal
-                && board[0][2] == player
-                && board[1][1] == player
-                && board[2][0] == player) {
+        if (
+            (board[selectedRow][0] == player && // 3-in-the-row
+                board[selectedRow][1] == player &&
+                board[selectedRow][2] == player) ||
+            (board[0][selectedCol] == player && // 3-in-the-column
+                board[1][selectedCol] == player &&
+                board[2][selectedCol] == player) ||
+            (selectedRow == selectedCol && // 3-in-the-diagonal
+                board[0][0] == player &&
+                board[1][1] == player &&
+                board[2][2] == player) ||
+            (selectedRow + selectedCol == 2 && // 3-in-the-opposite-diagonal
+                board[0][2] == player &&
+                board[1][1] == player &&
+                board[2][0] == player)
+        ) {
             return (player == CROSS) ? CROSS_WON : NOUGHT_WON;
         } else {
             // Nobody win. Check for DRAW (all cells occupied) or PLAYING.
@@ -135,7 +170,7 @@ public class TTTConsoleNonOO {
             for (int col = 0; col < COLS; ++col) {
                 paintCell(board[row][col]); // print each of the cells
                 if (col != COLS - 1) {
-                    System.out.print("|");   // print vertical partition
+                    System.out.print("|"); // print vertical partition
                 }
             }
             System.out.println();
@@ -149,9 +184,15 @@ public class TTTConsoleNonOO {
     /** Print a cell having the given content */
     public static void paintCell(int content) {
         switch (content) {
-            case CROSS:   System.out.print(" X "); break;
-            case NOUGHT:  System.out.print(" O "); break;
-            case NO_SEED: System.out.print("   "); break;
+            case CROSS:
+                System.out.print(" X ");
+                break;
+            case NOUGHT:
+                System.out.print(" O ");
+                break;
+            case NO_SEED:
+                System.out.print("   ");
+                break;
         }
     }
 }
